f7dcf568dd46248a3f44baceff8300e332888f85
roxanaloghin <roxana.loghin@gmail.com>
2022-10-26 00:03:57 +0300

    adding files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module",
+        "ecmaFeatures": {
+          "jsx": true
+        }
+      },
+
+    "rules": {
+    }
+}
